64813a3822c65cc485f4b6a5d7e0620e2ad4510c
Victor Fernandes <90485435+VictorFMag@users.noreply.github.com>
2024-01-22 10:10:45 -0300

    Delete Arvore-huffman directory

--- Arvore-huffman/NodoDaArvore.py
+++
@@ -1,7 +0,0 @@
-class NodoDaArvore:
-    def __init__(self, caractere, frequencia):
-        self.pai = None
-        self.esquerdo = None
-        self.direito = None
-        self.caractere = caractere
-        self.frequencia = frequencia
